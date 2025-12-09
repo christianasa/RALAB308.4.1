47f47a5cec20e997698b44591009edcd46b5653b
Christiana <Christiana@Christianas-MacBook-Air.local>
2025-12-09 16:22:40 -0700

    Steps 1 and Step 2

---
+++ script.js
@@ -0,0 +1,69 @@
+// Part 1-----------------------------------------------------------------------------------------------------
+// Refactoring Old Code
+
+let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+
+// Variables - to store data
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+// Cells track of commas / cells
+let commas = 0;
+
+// Looped over entire string.
+for (let i = 0; i < str.length; i++) {
+    //Saving value current char at the current index into a variable to make easier to use.
+    let current = str[i];
+
+    if (current == ",") {
+        // Move to the next cell
+        commas++;
+    } else if (current == "\n") {
+        console.log(cell1, cell2, cell3, cell4);
+        // Print all cells & reset cell/comma values for next line
+        commas = 0;
+        cell1 = "";
+        cell2 = "";
+        cell3 = "";
+        cell4 = "";
+    } else {
+        // For all other chars
+        // Add data to a cell
+        if (commas == 0) {
+            // If 0 commas add current char first cell
+            cell1 += current;
+        } else if (commas == 1) {
+            cell2 += current;
+        } else if (commas == 2) {
+            cell3 += current;
+        } else {
+            cell4 += current;
+        }
+    }
+
+    //Check if its the last index of loop.
+    if (i == str.length - 1) {
+        //if so, print last row
+        console.log(cell1, cell2, cell3, cell4);
+    }
+}
+
+// Part 2 -------------------------------------------------------------------------------------------------------------------
+// Expanding Functionality
+
+let rows = str.split('\n');
+console.log(",", rows);
+
+
+let array2D = [];
+
+for (let i = 0; i < rows.length; i++) {
+
+    let cells = rows[i].split(',');
+
+    array2D.push(cells);
+}
+
+console.log(",", array2D);
+
